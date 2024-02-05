58026557857ed27b9e738714a4859a6af5a176b8
Vaibhav Kanthi <vaibhav.kanthi@gmail.com>
2024-02-05 15:34:56 +0530

    Add student routes

--- server/src/routes/student-routes.ts
+++ server/src/routes/student-routes.ts
@@ -1,1 +1,13 @@
-import { Express } from "express";
+import { Router } from "express";
+import { getAllStudents, studentLogin, studentLogout, verifyStudent } from "../controllers/student-controller.js";
+import { loginValidator, validate } from "../utils/validator.js";
+import { verifyToken } from "../utils/token-manager.js";
+
+const studentRouter = Router();
+
+studentRouter.get("/", getAllStudents);
+studentRouter.post("/login", validate(loginValidator), studentLogin);
+studentRouter.get("/auth-status", verifyToken, verifyStudent);
+studentRouter.get("/logout", verifyToken, studentLogout);
+
+export default studentRouter;
